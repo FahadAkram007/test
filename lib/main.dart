ac8fec26c998aa4872091b9ac6819d8b6d951de1
FahadAkram007 <fahad@herrmann-lausitz.de>
2026-09-23 10:50:29 +0200

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ class MyApp extends StatelessWidget {
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      //content
     );
   }
 }
